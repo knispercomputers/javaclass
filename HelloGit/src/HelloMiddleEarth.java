800b434a16efa5fa601adff79c365bb7b0945aeb
Teemu <nospam@gmail.com>
2018-08-02 22:12:06 -0400

    added and print out and two new characters

--- HelloGit/src/HelloMiddleEarth.java
+++ HelloGit/src/HelloMiddleEarth.java
@@ -3,11 +3,12 @@ public class HelloMiddleEarth {
 	
 	public static void main(String[] args) {
 		
-		String[] names = { "Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf" };
+		String[] names = { "Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf", "Boromir", "Faramir" };
 		
 		// print greetings
 		for (int i = 0; i<names.length; i++) {
 			System.out.format("Hello, %s!\n", names[i]);
+			System.out.println("How are you doing today?");
 		}
 	}
 
